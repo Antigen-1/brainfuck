2442dcbc62632677115c3a7c17ecbc3174220597
ZhangHao <zhanghao@antigen.top>
2024-06-18 16:44:23 +0800

    Fix the reader

--- main.rkt
+++ main.rkt
@@ -2,6 +2,7 @@
 
 (module reader syntax/module-reader
   brainfuck/expander/expander
-  #:read bf:read
-  #:read-syntax bf:read-syntax
+  #:read (compose1 list bf:read)
+  #:read-syntax (compose1 list bf:read-syntax)
+  #:whole-body-readers? #t
   (require (prefix-in bf: "reader/reader.rkt")))
