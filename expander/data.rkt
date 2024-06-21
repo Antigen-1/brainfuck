027897339f029930905d8bc88db4cee4fb48247d
ZhangHao <zhanghao@antigen.top>
2024-06-21 11:08:00 +0800

    Use unsafe functions

--- expander/data.rkt
+++ expander/data.rkt
@@ -1,20 +1,20 @@
 #lang racket/base
-(require (submod racket/performance-hint begin-encourage-inline))
+(require (submod racket/performance-hint begin-encourage-inline)
+         racket/unsafe/ops)
 (provide add sub shiftr shiftl read put cur n:+)
 
 ;; former current latter
 ;; (vector/c list? byte? list?)
 (define main-vector (vector null 0 null))
-
-(define-syntax-rule (make-mutator-and-accessor vec ind)
+(define-syntax-rule (make-observer ind)
   (case-lambda
-    (() (vector-ref vec ind))
-    ((v) (vector-set! vec ind v))))
+    (() (unsafe-vector-ref main-vector ind))
+    ((v) (unsafe-vector-set! main-vector ind v))))
 
 (begin-encourage-inline
-  (define cur (make-mutator-and-accessor main-vector 1))
-  (define former (make-mutator-and-accessor main-vector 0))
-  (define latter (make-mutator-and-accessor main-vector 2))
+  (define cur (make-observer 1))
+  (define former (make-observer 0))
+  (define latter (make-observer 2))
 
   (define (n:+ v1 v2)
    (bitwise-and (+ v1 v2) 255))
@@ -30,16 +30,16 @@
                (cur c)
                (latter l))
         (if (null? f)
-            (shiftr (sub1 n) null 0 (cons c l))
-            (shiftr (sub1 n) (cdr f) (car f) (cons c l)))))
+            (shiftr (sub1 n) null 0 (unsafe-cons-list c l))
+            (shiftr (sub1 n) (unsafe-cdr f) (unsafe-car f) (unsafe-cons-list c l)))))
   (define (shiftl n (f (former)) (c (cur)) (l (latter)))
     (if (zero? n)
         (begin (former f)
                (cur c)
                (latter l))
         (if (null? l)
-            (shiftl (sub1 n) (cons c f) 0 null)
-            (shiftl (sub1 n) (cons c f) (car l) (cdr l)))))
+            (shiftl (sub1 n) (unsafe-cons-list c f) 0 null)
+            (shiftl (sub1 n) (unsafe-cons-list c f) (unsafe-car l) (unsafe-cdr l)))))
   (define (read n)
     (define (->byte v)
       (if (eof-object? v)
